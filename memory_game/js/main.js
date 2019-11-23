031013d338e36763017246ed357a9e24fac90fa0
ptds106 <58052333+ptds106@users.noreply.github.com>
2019-11-22 20:52:43 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,10 @@
 // console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
 console.log("user flipped " + cardOne);
 console.log("user flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInplay.push(cardOne)
+var cardTwo = cards[2];
+cardsInplay.push(cardTwo)
