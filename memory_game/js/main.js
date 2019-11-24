a76b217204348191d422e71525771fc6dd1d7107
ptds106 <58052333+ptds106@users.noreply.github.com>
2019-11-24 00:49:47 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -18,11 +18,7 @@ var cards = [
 	rank: "king",
 	suit: "diamonds",
 	cardImage: "images/king-of-diamonds.png",
-}
-];
-
-
-
+}];
 
 var cardsInPlay = [];
 var checkForMatch = function()
@@ -36,19 +32,32 @@ var checkForMatch = function()
 		alert("Sorry, try again.");
 	}
 }
-var flipCard = function(cardId)
+
+var flipCard = function()
 {
+	let cardId = this.getAttribute('data-id')
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage)
 	console.log(cards[cardId].suit)
+	this.setAttribute('src', cards[cardId].cardImage)
 	if(cardsInPlay.length === 2)
 	{
 		checkForMatch();
-	}
-	
-	
+	}	
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function()
+{
+	for(let i = 0; i < cards.length; i++)
+	{
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute('alt', 'chosen image');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);	
+		document.getElementById('game-board').appendChild(cardElement)	
+	}
+	console.log(cardElement)
+}
+createBoard();
